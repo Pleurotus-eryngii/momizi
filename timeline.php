a2ca71066ccdf4b6105dcd241d4976602b473f7c
Kanade Toshiyuki <come25136@gmail.com>
2017-01-11 01:00:15 +0900

    [add] timeline.php
    ログイン時のリダイレクト先ファイル

---
+++ timeline.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+
+    <title>Momizi - timeline</title>
+
+    <?php require __DIR__ . '/favicon.php'; ?>
+</head>
+
+<body>
+
+</body>
+
+</html>
